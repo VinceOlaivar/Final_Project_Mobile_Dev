99c51e5ed0ee264e95a7dd3d0731b2e134903d2f
Vince <olaivar.v@yahoo.com>
2025-11-22 22:46:21 +0800

    commit d4f5e6b7c8d9e0f1a2b3c4d5e6f7g8h9i0j1k2l3 Author: vince_Olaivar <

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,34 @@
+import 'package:final_project/components/my_textfield.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.message, 
+              size: 60, 
+              color: Theme.of(context).colorScheme.primary),
+          const SizedBox(height: 20),
+          Text(
+            'Welcome to the Login Page',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 16,
+            ),
+          ),
+
+          const SizedBox(height: 20),
+
+          MyTextfield(),
+
+        ],
+      )
+    );
+  }
+}
